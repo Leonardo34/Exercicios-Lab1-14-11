f84eea3bbc4e70a4eead33db499934912894cfd5
Leonardo34 <leo_broch@hotmail.com>
2016-11-14 20:36:16 -0200

    Adicionado método ordenaAlfabeticamente() na Classe Lista12

--- Exercicios-Lab1/Lista12.java
+++ Exercicios-Lab1/Lista12.java
@@ -2,15 +2,32 @@
 public class Lista12
 {
     public byte[] ordenaCrescente (byte[] array) {
+        byte aux;
         for (int i =0; i < array.length - 1; i++) {
             for(int j = i + 1; j < array.length; j++) {
                 if(array[i] > array[j]) {
-                    byte aux = array[i];
+                    aux = array[i];
                     array[i] = array[j];
                     array[j] = aux;
                 }
             }
         }
         return array;
-    } 
+    }
+    
+    public String[] ordenaAlfabeticamente (String[] words) {
+        String aux;
+        for (int i = 0; i < words.length - 1; i++) {
+            for(int j = i + 1; j < words.length; j++) {
+                if (words[i] != null && words[j] != null) {
+                    if (words[i].compareTo(words[j]) > 0) {
+                        aux = words[i];
+                        words[i] = words[j];
+                        words[j] = aux;
+                    }
+                }
+            }
+        }
+        return words;
+    }
 }
