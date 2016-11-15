b3fa9312d0245cadef284985d1bd898c66a601a3
Leonardo34 <leo_broch@hotmail.com>
2016-11-15 10:46:04 -0200

    Criado atributos e construtor da Classe Loja

--- Exercicios-Lab1/Loja.java
+++ Exercicios-Lab1/Loja.java
@@ -1,33 +1,24 @@
 
-/**
- * Escreva a descrição da classe Loja aqui.
- * 
- * @author (seu nome) 
- * @version (número de versão ou data)
- */
 public class Loja
 {
-    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
-    private int x;
-
-    /**
-     * COnstrutor para objetos da classe Loja
-     */
-    public Loja()
-    {
-        // inicializa variáveis de instância
-        x = 0;
+    private Pessoa[] p;
+    
+    public Loja(int tamanho) {
+        p = new Pessoa[tamanho];
+        int r = 0, count = 0;
+        for (int i = 0; i < p.length; i++) {
+            int ano = 1940 + (int) (Math.random() * 73);
+            r = 1 + (int) (Math.random() * 2);
+            
+            if(r == 1) {
+                p[i] = new Pessoa ("P" + count++, ano);
+            }
+            else {
+                p[i] = new Cliente ("C" + count++, ano, (int) (Math.random() * 1000));
+            }
+        }
     }
-
-    /**
-     * Exemplo de método - substitua este comentário pelo seu próprio
-     * 
-     * @param  y   exemplo de um parâmetro de método
-     * @return     a soma de x com y 
-     */
-    public int sampleMethod(int y)
-    {
-        // ponha seu código aqui
-        return x + y;
-    }
+    
+    public Pessoa[] getP() {return p;}
+    
 }
